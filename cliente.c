fa85eb393af16abde727ad31da13682015a12d24
Mirkopoj <mirkopoj@hotmail.com>
2022-06-16 17:38:56 -0300

    Primer commit experimental

--- cliente.c
+++ cliente.c
@@ -3,8 +3,13 @@
 #include <stdlib.h>
 #include <sys/unistd.h>
 #include <netdb.h>              
+#include <pthread.h>
+#include <unistd.h>
 
 #define MAX_DATA 103   
+
+void stress_test();
+void *thread_cliente(void *a);
 
 int main(int argc, char *argv[]){
 
@@ -35,8 +40,11 @@ int main(int argc, char *argv[]){
 			case 2:
 				PORT = 3551;
 				break;
+			case 3:
+				stress_test(argv[1]);
+				break;
 			default:
-				printf("CHE ヽ(ಠ_ಠ)ノ, te dije 1 o 2... Porfa elegi uno válido.\n");
+				printf("CHE ヽ(ಠ_ಠ)ノ, te dije 1, 2 o 3... Porfa elegi uno válido.\n");
 				opc = 0;
 				break;
 		}	
@@ -108,3 +116,81 @@ int main(int argc, char *argv[]){
 	//se cierra el socket
 	close(sockid);   
 }
+
+void stress_test(void *ip){
+	pthread_t clientes[40];
+	for(int i=0;i<40;i++){
+		pthread_create(&clientes[i], NULL, thread_cliente, ip);
+	}
+	for(int i=0;i<40;i++){
+		pthread_join(clientes[i],0);
+	}
+	exit(0);
+}
+
+void *thread_cliente(void * ip){
+	int sockid, numbytes,n, opc, PORT; //Fichostdataros descriptores y definiciones       
+	char str[5];
+	char buf[MAX_DATA]; //almacen de textos recibidos -> en BYTES  
+	struct hostent *hostdata; //struct info sobre el host
+	struct sockaddr_in servidor; //info direcciones del servidor para la conexión  
+	//llamada a gethostbyname() obtengo info del servidor a partir de su IP
+   	if ((hostdata=gethostbyname((const char *)ip))==NULL){       
+      		printf("Error en la dirección IP\n");
+      		exit(-1);
+   	}
+	for (int locura=0;locura<1000;locura++) {
+   	//llamada a socket(), AF_INET es el dominio (IPv4), SOCK_STREAM nos aseguramos conexión TCP
+   	if ((sockid=socket(AF_INET, SOCK_STREAM, 0))==-1){  
+      		printf("Error en llamar al socket\n");
+      		exit(-1);
+   	}
+	
+		//datos del servidor
+		servidor.sin_family = AF_INET;
+		PORT = 3551 + (rand()%2);
+		servidor.sin_port = htons(PORT); 	
+		servidor.sin_addr = *((struct in_addr *)hostdata->h_addr); //*hostdata->h_addr pasa la información de *hostdata a h_addr 
+		bzero(&(servidor.sin_zero), sizeof(servidor.sin_zero));
+
+			//llamada a connect(), para ppoder conectarse al servidor
+			if(connect(sockid, (struct sockaddr *)&servidor, sizeof(struct sockaddr))==-1){ 
+				printf("Error al conectar con el servidor\n");
+		  		exit(-1);
+			}
+	
+			//las siguientes dos funciones manejan la comunicación cliente-servidor
+	
+			//llamada a recv(), espera un mensaje del servidor
+			if ((numbytes=recv(sockid,buf,MAX_DATA,0)) == -1){  
+					printf("Error al recibir \n");
+					exit(-1);
+			}
+	
+			int muchas_veces = rand()%1000;
+		  	for(int veces=0;veces<muchas_veces;veces++) {
+			if (send(sockid, "a", 1, 0) == -1) {
+				perror("send");
+				exit(1);
+			}
+			if ((n=recv(sockid, str, 3, 0)) > 0) {
+				str[n] = '\0';
+				printf("Tú número es: %s \n", str);
+			} else {
+				if (n < 0){
+					perror("recv");
+				} else {
+					printf("¡De malas ლ(ಠ_ಠლ)!El servidor ha finalizado su conexión\n");
+				}
+				close(sockid);
+			     exit(-1);
+		 	}
+		}
+		//se cierra el socket
+		close(sockid);
+	}
+	pthread_exit(0);
+}
+
+
+
